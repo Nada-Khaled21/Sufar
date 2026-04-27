315e5e5df61820804112d12bc87ce06f4ebcffac
Nada_Khaled21 <nadak4540@gmail.com>
2026-04-27 22:53:09 +0300

    fix: vercel serverless support

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,17 +1,17 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, htmlContent) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
+      host: "smtp.gmail.com",
       port: 465,
       secure: true, // Port 465 uses secure: true
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        pass: process.env.EMAIL_PASS,
       },
       // إضافة المهلة عشان Vercel يستنى شوية
-      connectionTimeout: 10000, 
+      connectionTimeout: 10000,
       greetingTimeout: 10000,
     });
 
@@ -19,15 +19,14 @@ const sendEmail = async (to, subject, htmlContent) => {
       from: `"Sufar Travel" <${process.env.EMAIL_USER}>`,
       to: to,
       subject: subject,
-      html: htmlContent
+      html: htmlContent,
     });
 
     console.log(`✅ Success! Email ID: ${info.messageId}`);
     return true;
-
   } catch (error) {
     console.error(`❌ Email Error: ${error.message}`);
-    throw error;
+    return false;
   }
 };
 
